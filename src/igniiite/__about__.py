9d14c62dc894ecc551fbf32ce06d83ca8c7ed150
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2024-10-06 15:44:10 +0200

    Add hatch environment for documentation environment and lint

---
+++ src/igniiite/__about__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2024-present Florian Dupeyron <florian.dupeyron@mugcat.fr>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+__author__ = "Florian Dupeyron"
+__email__ = "florian.dupeyron@mugcat.fr"
+__version__ = "0.1.0"
--- src/igniiite/__init__.py
+++ src/igniiite/__init__.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024-present Florian Dupeyron <florian.dupeyron@mugcat.fr>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
 """
 =================================================
 Igniiite! Simple init framework written in python
